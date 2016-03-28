399dcf7a65154f3bd479559c78810da753e0bb11
AlexeySavanchuk <fktrctq1852@gmail.com>
2016-03-28 13:00:27 +0300

    testing js

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
-<?php  echo " github second commit"; ?>
-<?php  echo "=D" ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<meta charset="utf-8"/>	
+</head>
+<body>
+<script type="text/javascript" src="/javascript/main.js"></script>
+   <!--	<div class="slider-container">
+   		<div class="slider-wrapper">
+   			<div class="slide"></div>
+   			<div class="slider-nav">
+   				<button id="firstButton" class="slider-previous" name="Назад"></button>
+   				<button class="slider-next" name="Вперед"></button>
+   			</div>
+   		</div>
+   	</div> -->
+<p>Это <img src="http://cs631928.vk.me/v631928952/1cb87/Yb5bqmPP154.jpg" alt="Алина"> и 
+       <img src="http://cs629519.vk.me/v629519084/36ea1/wRUi-iUtF18.jpg" alt="Лиза" >, классные девчонки</p>
+<p><button onclick="function_name()">Заменить</button></p>
+<script type="text/javascript">CheckAge();</script>
+</body>
+</html>
